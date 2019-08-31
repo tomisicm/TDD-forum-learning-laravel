c59ea01c73e24207d8aa54f6c03dcc9db90339f0
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-08-31 18:52:08 +0200

    comments for thraed

--- app/Thread.php
+++ app/Thread.php
@@ -2,29 +2,73 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
     protected $fillable = ['title', 'body', 'channel_id', 'user_id'];
 
+    //
+    protected static function boot()
+    {
+        parent::boot();
+
+
+        static::addGlobalScope('replyCount', function (Builder $builder) {
+            $builder->withCount('replies');
+        });
+    }
+
+    /**
+     * A thread may have many replies.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function replies()
     {
         return $this->hasMany(Reply::class);
     }
 
+    /**
+     * A thread belongs to a creator.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function creator()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    /**
+     * A thread is assigned a channel.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function channel()
     {
         return $this->belongsTo(Channel::class, 'channel_id');
     }
 
+    /**
+     * Add a reply to the thread.
+     *
+     * @param $reply
+     */
     public function add_reply($reply)
     {
         $this->replies()->create($reply);
+    }
+
+    /**
+     * Apply all relevant thread filters.
+     *
+     * @param  Builder       $query
+     * @param  ThreadFilters $filters
+     * @return Builder
+     */
+    public function scopeFilter($query, $filters)
+    {
+        return $filters->apply($query);
     }
 }
