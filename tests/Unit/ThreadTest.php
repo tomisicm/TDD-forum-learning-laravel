287e3af6fe81d45aa1e2b4c571a7f33c9d2040d1
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-02 19:06:36 +0200

    ayy

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -48,12 +48,4 @@ class ThreadTest extends TestCase
 
         $this->assertCount(1, $thread->replies);
     }
-
-    // /** @test */
-    // public function a_thread_can_make_a_string_path()
-    // {
-    //     $thread = factory(Thread::class)->create();
-
-    //     $slug = "/threads/{$thread->channel->slug}/$thread->id";
-    // }
 }
