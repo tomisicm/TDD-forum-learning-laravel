a01221db6c83696f4524c36648caaaed9ce66574
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-04 07:55:21 +0200

    favorited

--- app/Favorite.php
+++ app/Favorite.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
+    use RecordsActivity;
+
     protected $fillable = ['user_id', 'favorited_id', 'favorited_type'];
+
+    public function favorited()
+    {
+        return $this->morphTo();
+    }
 }
