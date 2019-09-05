00b4463f5c64794f01dc3475dd198849e07bb1a6
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-05 12:03:17 +0200

    favouritable trait

--- app/Favouritable.php
+++ app/Favouritable.php
@@ -4,5 +4,34 @@ namespace App;
 
 trait Favouritable
 {
-    // TODO
+    public function favorites()
+    {
+        return $this->morphMany(Favorite::class, 'favorited');
+    }
+
+    // TODO: counting
+    // /**
+    //  * Get the number of favorites for the reply.
+    //  *
+    //  * @return integer
+    //  */
+    // public function getFavoritesCountAttribute()
+    // {
+    //     return $this->favorites->count();
+    // }
+
+    public function favorite($user)
+    {
+        $attributes = ['user_id' => $user];
+
+        if (!$this->isFavorited($user)) {
+            return $this->favorites()->create($attributes);
+        }
+    }
+
+    public function isFavorited($user)
+    {
+        // return $this->favorites()->where(['user_id' => $user])->exists();
+        return !!$this->favorites->where(['user_id' => $user])->count();
+    }
 }
--- app/Reply.php
+++ app/Reply.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    use RecordsActivity;
+    use RecordsActivity, Favouritable;
 
     protected $fillable = ['thread_id', 'user_id', 'body'];
 
@@ -33,35 +33,4 @@ class Reply extends Model
     {
         return $this->belongsTo(User::class, 'user_id');
     }
-
-    public function favorites()
-    {
-        return $this->morphMany(Favorite::class, 'favorited');
-    }
-
-    public function favorite($user)
-    {
-        $attributes = ['user_id' => $user];
-
-        if (!$this->isFavorited($user)) {
-            return $this->favorites()->create($attributes);
-        }
-    }
-
-    public function isFavorited($user)
-    {
-        // return $this->favorites()->where(['user_id' => $user])->exists();
-        return !!$this->favorites->where(['user_id' => $user])->count();
-    }
-
-    // TODO: counting
-    // /**
-    //  * Get the number of favorites for the reply.
-    //  *
-    //  * @return integer
-    //  */
-    // public function getFavoritesCountAttribute()
-    // {
-    //     return $this->favorites->count();
-    // }
 }
