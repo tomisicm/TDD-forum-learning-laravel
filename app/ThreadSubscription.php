7cb383ca0ad4538fa9ccebf2424fe413da186301
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-07 20:57:04 +0200

    just lil tweak

--- app/ThreadSubscription.php
+++ app/ThreadSubscription.php
@@ -15,8 +15,13 @@ class ThreadSubscription extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
+
     public function notify($reply)
     {
-        $this->user->notify(new ThreadWasUpdated($reply->thread, $reply));
+        $this->user->notify(new ThreadWasUpdated($this->thread, $reply));
     }
 }
