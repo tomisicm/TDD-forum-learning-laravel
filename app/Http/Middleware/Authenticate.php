a4cc8953be998eb83300e3b8f567bf8e16e515d7
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-14 09:45:54 +0200

    fsgbv

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -14,8 +14,9 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        // stupid redirection
+        // if (! $request->expectsJson()) {
+        //     return route('login');
+        // }
     }
 }
