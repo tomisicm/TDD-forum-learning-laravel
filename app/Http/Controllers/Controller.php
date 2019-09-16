ec5dd18a8b19be1f04b2360090b8b0598d59c9f9
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-16 08:25:45 +0200

    3 fixed

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,17 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class Controller extends BaseController
 {
+    // https://stackoverflow.com/questions/31309098/laravel-testcase-not-sending-authorization-headers-jwt-token
+    public function __construct()
+    {
+        if ((\App::environment() == 'testing') && array_key_exists("HTTP_AUTHORIZATION",  \Request::server())) {
+            JWTAuth::setRequest(\Route::getCurrentRequest());
+        }
+    }
+
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
