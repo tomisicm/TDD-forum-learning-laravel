e44fc7308e8927086d34066cac8be798f696bab4
tomisicm <41255622+tomisicm@users.noreply.github.com>
2019-09-30 08:03:25 +0200

    fixing migrations

--- database/migrations/2019_08_25_172825_create_threads_table.php
+++ database/migrations/2019_08_25_172825_create_threads_table.php
@@ -22,7 +22,7 @@ class CreateThreadsTable extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            // $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
+            $table->foreign('channel_id')->references('id')->on('channels')->onDelete('cascade');
         });
     }
 
